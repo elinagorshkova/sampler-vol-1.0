c8ce78db68c1589905f048bfc51cab4265c5288f
elinag <elina.gorshkova@icloud.com>
2019-11-16 02:08:56 -0500

    Moved some JS functions from ui.js file

--- assets/scripts/uploads/api.js
+++ assets/scripts/uploads/api.js
@@ -1,6 +1,7 @@
 'use strict'
 const config = require('../config')
 
+// Send form data to the back end where it will be processed and uploaded to AWS
 const upload = function (formData) {
   return $.ajax({
     url: `${config.apiUrl}/fileUploads`,
--- assets/scripts/uploads/events.js
+++ assets/scripts/uploads/events.js
@@ -6,35 +6,40 @@ const store = require('../store')
 
 const addHandlers = () => {
   $('.file-upload').on('submit', onCreateUpload)
-  $('.file-upload-update').on('submit', onCreateUploadUpdate)
+  $('.file-upload-update').on('submit', onUpdateUpload)
 }
 
 const onCreateUpload = function (event) {
   // prevent default page reload
-  const id = event.target.name
   event.preventDefault()
+  // getting name of the clicked element, which is corresponding with sound index in the sounds array
+  const index = event.target.name
   // Get Data from the form
   const data = new FormData(event.target)
   api.upload(data)
     .then(data => {
+      // getting AWS url to the uploaded file
       const url = data.fileUpload.url
-      // store.sounds.splice(id, 0, url)
-      store.sounds[id] = url
+      // Adding the url to the sound array on corresponding index
+      store.sounds[index] = url
       return data
     })
     .then(ui.uploadSucces)
     .catch(console.error)
 }
 
-const onCreateUploadUpdate = function (event) {
+const onUpdateUpload = function (event) {
   // prevent default page reload
-  const index = store.soundIndex
   event.preventDefault()
+  // We stored the soundIndex earlier from the deopdown input in Update form
+  const index = store.soundIndex
   // Get Data from the form
   const data = new FormData(event.target)
   api.upload(data)
     .then(data => {
+      // getting AWS url to the uploaded file
       const url = data.fileUpload.url
+      // Replacing the old url with the new url
       store.sounds[index] = url
       return data
     })
